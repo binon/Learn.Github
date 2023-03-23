a28b4502c21e20f5a9ef25dec61acc76dfa72912
binon <binon_prabu@yahoo.com>
2023-03-23 21:17:45 +0000

    Add project files.

---
+++ Controllers/LearnController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Learn.Github.Controllers
+{
+    public class LearnController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Learn.Github.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
